ff20a7aba1078a7ed62ee5ce7eb4fbd5ce54a38e
Vitaliy <Xytc-1215@mail.ru>
2025-06-24 13:46:15 +0300

    Добавил реалзацию Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/gittest2";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "springcourse";
 
+// JDBC
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -20,6 +27,43 @@ public class Util {
         }
         return connection;
     }
+
+    // Hibernate
+    private static SessionFactory sessionFactory;
+
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                        .applySettings(getHibernateProperties())
+                        .build();
+
+                sessionFactory = new MetadataSources(registry)
+                        .addAnnotatedClass(jm.task.core.jdbc.model.User.class)
+                        .buildMetadata()
+                        .buildSessionFactory();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (sessionFactory != null) {
+                    sessionFactory.close();
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    // Настройки Hibernate в Properties
+    private static Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.connection.url", URL);
+        properties.put("hibernate.connection.username", USERNAME);
+        properties.put("hibernate.connection.password", PASSWORD);
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "update"); // автоматически обновляет структуру таблиц
+        return properties;
+    }
 }
-
-
